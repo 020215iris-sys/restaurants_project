c00281f7778c586e3e35f1d0db5e89d364328392
sjs33az <dhdnjsfjqm@example.com>
2026-05-12 09:34:15 +0900

    분식 카테고리 추가

---
+++ restaurants/snack.py
@@ -0,0 +1,10 @@
+# restaurants/snack.py
+
+snack_restaurants = [
+    {"name": "떡볶이 천국", "rating": 4.7, "price": 4000},
+    {"name": "김밥 한줄", "rating": 4.0, "price": 3500},
+]
+
+def get_snack():
+    """분식 맛집 목록을 반환한다."""
+    return snack_restaurants
