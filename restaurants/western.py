3e95a5fdac093f4d73069e961fb92c2d495f9a53
restaurants <orolroom@gmail.com>
2026-05-12 09:51:39 +0900

    western.py 추가

---
+++ restaurants/western.py
@@ -0,0 +1,8 @@
+western_food_list = [
+    {"name": "파스타 하우스", "rating": 4.6, "price": 15000},
+    {"name": "스테이크 키친", "rating": 4.9, "price": 32000},
+]
+
+def get_western_food():
+    """양식 음식점 목록을 반환한다."""
+    return western_food_list
